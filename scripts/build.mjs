d06152fd399c651cfe17ce3338ea217de5df3c09
david650991 <david650991@gmail.com>
2026-09-02 12:05:43 -0600

    fix: correct homepage canonical URL

--- scripts/build.mjs
+++ scripts/build.mjs
@@ -61,7 +61,7 @@ function schema() {
 }
 
 function layout({title, description, file, content}) {
-  const canonical = absolute(file);
+  const canonical = absolute(file === 'index.html' ? '' : file);
   return `<!doctype html><html lang="es-MX"><head>
   <meta charset="utf-8"><meta name="viewport" content="width=device-width, initial-scale=1">
   <title>${escape(title)}</title><meta name="description" content="${escape(description)}"><meta name="author" content="${escape(author.publicName)}">
